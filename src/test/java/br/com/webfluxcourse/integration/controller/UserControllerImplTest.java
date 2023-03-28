c39b2e918a51cdde1da1be73c2c4b13643d2a6e6
Felipe Campos <felipecamposlv@gmail.com>
2023-03-28 16:55:35 -0300

    Test integration save badrequest userController

--- src/test/java/br/com/webfluxcourse/integration/controller/UserControllerImplTest.java
+++ src/test/java/br/com/webfluxcourse/integration/controller/UserControllerImplTest.java
@@ -20,6 +20,9 @@ import org.springframework.web.reactive.function.BodyInserters;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.web.reactive.function.BodyInserters.fromValue;
 import static reactor.core.publisher.Mono.just;
 import static reactor.core.publisher.Mono.when;
 
@@ -27,6 +30,12 @@ import static reactor.core.publisher.Mono.when;
 @SpringBootTest
 @AutoConfigureWebTestClient
 class UserControllerImplTest {
+
+    private static final String ID = "123456";
+    private static final String NAME = "Tekomu Nakama";
+    private static final String EMAIL = "tekomu.nakama2@mail.com";
+    private static final String PASSWORD = "123";
+    private static final String BASE_URI = "/users";
 
     @Autowired
     private WebTestClient webTestClient;
@@ -43,17 +52,37 @@ class UserControllerImplTest {
     @Test
     @DisplayName("Test endpoint save with success")
     void save() {
-        final var request = new UserRequest("Tekomu Nakama", "tekomu.nakama2@mail.com", "123");
+        final var request = new UserRequest(NAME, EMAIL, PASSWORD);
 
         when(userService.save(any(UserRequest.class))).thenReturn(just(User.builder().build()));
 
-        webTestClient.post().uri("/users")
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(BodyInserters.fromValue(request))
+        webTestClient.post().uri(BASE_URI)
+                .contentType(APPLICATION_JSON)
+                .body(fromValue(request))
                 .exchange()
                 .expectStatus().isCreated();
 
         verify(userService, times(1)).save(any(UserRequest.class));
+    }
+
+    @Test
+    @DisplayName("Test endpoint save with bad request")
+    void testSaveWithBadRequest() {
+        final var request = new UserRequest(NAME.concat(" "), EMAIL, PASSWORD);
+
+        webTestClient.post().uri(BASE_URI)
+                .contentType(APPLICATION_JSON)
+                .body(fromValue(request))
+                .exchange()
+                .expectStatus().isBadRequest()
+                .expectBody()
+                .jsonPath("$.path").isEqualTo(BASE_URI)
+                .jsonPath("$.status").isEqualTo(BAD_REQUEST.value())
+                .jsonPath("$.error").isEqualTo("Validation error")
+                .jsonPath("$.message").isEqualTo("Error on validation attributes")
+                .jsonPath("$.errors[0].fieldName").isEqualTo("name")
+                .jsonPath("$.errors[0].message").isEqualTo("field cannot have blank spaces at the beginning or at end");
+
     }
 
     @Test
