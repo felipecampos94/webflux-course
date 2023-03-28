2de566572f389dabf1fe57fc16d79ef7780858a2
Felipe Campos <felipecamposlv@gmail.com>
2023-03-28 12:53:50 -0300

    Add mapper to user and impl to save contract user controller

---
+++ src/main/java/br/com/webfluxcourse/controller/impl/UserControllerImpl.java
@@ -0,0 +1,47 @@
+package br.com.webfluxcourse.controller.impl;
+
+import br.com.webfluxcourse.controller.UserController;
+import br.com.webfluxcourse.entity.model.request.UserRequest;
+import br.com.webfluxcourse.entity.model.response.UserResponse;
+import br.com.webfluxcourse.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping(value = "/users")
+@RequiredArgsConstructor
+public class UserControllerImpl implements UserController {
+
+    private final UserService userService;
+
+    @Override
+    public ResponseEntity<Mono<Void>> save(UserRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(userService.save(request).then());
+    }
+
+    @Override
+    public ResponseEntity<Mono<UserResponse>> findById(String id) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Flux<UserResponse>> findAll() {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Mono<UserResponse>> update(String id, UserRequest request) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Mono<Void>> delete(String id) {
+        return null;
+    }
+}
---
+++ src/main/java/br/com/webfluxcourse/service/UserService.java
@@ -0,0 +1,21 @@
+package br.com.webfluxcourse.service;
+
+import br.com.webfluxcourse.entity.User;
+import br.com.webfluxcourse.entity.model.request.UserRequest;
+import br.com.webfluxcourse.mapper.UserMapper;
+import br.com.webfluxcourse.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final UserMapper mapper;
+
+    public Mono<User> save(final UserRequest request) {
+        return userRepository.save(mapper.toEntity(request));
+    }
+}
