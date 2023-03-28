0d54d2e2a3faf3ef3c7411fa508d7927f0fc634e
Felipe Campos <felipecamposlv@gmail.com>
2023-03-28 14:19:04 -0300

    Add string validator

--- src/main/java/br/com/webfluxcourse/entity/model/request/UserRequest.java
+++ src/main/java/br/com/webfluxcourse/entity/model/request/UserRequest.java
@@ -1,17 +1,19 @@
 package br.com.webfluxcourse.entity.model.request;
 
+import br.com.webfluxcourse.validator.TrimString;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record UserRequest(
-
-        @Size(min = 3, max = 50, message = "Must Be Between 3 and 50 characters")
+        @TrimString @Size(min = 3, max = 50, message = "Must Be Between 3 and 50 characters")
         @NotBlank(message = "Must Not Be Null Empty")
         String name,
+        @TrimString
         @Email(message = "Invalid Email")
         @NotBlank(message = "Must Not Be Null Empty")
         String email,
+        @TrimString
         @Size(min = 3, max = 50, message = "Must Be Between 3 and 50 characters")
         @NotBlank(message = "Must Not Be Null Empty")
         String password
